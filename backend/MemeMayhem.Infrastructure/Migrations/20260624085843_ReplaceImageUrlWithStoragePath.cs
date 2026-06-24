8629ce37686d2f43a7219e60f4194b426c1b4534
Brian Kabbo <159421821+briankabbo@users.noreply.github.com>
2026-06-24 14:59:56 +0600

    feat: rename ImageUrl to StoragePath in MemeCards table

---
+++ backend/MemeMayhem.Infrastructure/Migrations/20260624085843_ReplaceImageUrlWithStoragePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemeMayhem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceImageUrlWithStoragePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "MemeCards",
+                newName: "StoragePath");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StoragePath",
+                table: "MemeCards",
+                newName: "ImageUrl");
+        }
+    }
+}
